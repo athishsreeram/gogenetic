ea7eb9f8918e16ace2de700525e4cfd310c7ada4
anharay <athishsreeram.nharayanan@bmo.com>
2019-05-04 02:37:40 -0400

    remove swagger generation use grpcgw swagger instead

--- gogenetic-run.sh
+++ gogenetic-run.sh
@@ -1,4 +1,3 @@
-go run ./gen/gogentic.go -tomlFile=/Users/anharay/go/src/gogenetic/toml/test.toml -templateFile=/Users/anharay/go/src/gogenetic/template/swaggertemplate.goproto
 go run ./gen/gogentic.go -tomlFile=/Users/anharay/go/src/gogenetic/toml/test.toml -templateFile=/Users/anharay/go/src/gogenetic/template/proto-grpcresttemplate.goproto 
 go run ./gen/gogentic.go -tomlFile=/Users/anharay/go/src/gogenetic/toml/test.toml -templateFile=/Users/anharay/go/src/gogenetic/template/servicetemplate.goproto
 go run ./gen/gogentic.go -tomlFile=/Users/anharay/go/src/gogenetic/toml/test.toml -templateFile=/Users/anharay/go/src/gogenetic/template/grpcservertemplate.goproto

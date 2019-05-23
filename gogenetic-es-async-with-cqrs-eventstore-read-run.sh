67113aa0fa6c4c7dd3f3642c83c022fafa44c89c
anharay <athishsreeram.nharayanan@bmo.com>
2019-05-23 16:22:37 -0400

    Event Source read output

---
+++ gogenetic-es-async-with-cqrs-eventstore-read-run.sh
@@ -0,0 +1,31 @@
+go run ./gen/gogentic.go -tomlFile=/Users/anharay/go/src/gogenetic/toml/eventstore-read.toml -templateFile=/Users/anharay/go/src/gogenetic/template/go/es-cqrs-read/proto-grpcresttemplate.tpl -outDir=eventstore-es-read-output
+go run ./gen/gogentic.go -tomlFile=/Users/anharay/go/src/gogenetic/toml/eventstore-read.toml -templateFile=/Users/anharay/go/src/gogenetic/template/go/es-cqrs-read/servicecrudtemplate.tpl -outDir=eventstore-es-read-output
+go run ./gen/gogentic.go -tomlFile=/Users/anharay/go/src/gogenetic/toml/eventstore-read.toml -templateFile=/Users/anharay/go/src/gogenetic/template/go/es-cqrs-read/grpcservertemplate.tpl -outDir=eventstore-es-read-output
+go run ./gen/gogentic.go -tomlFile=/Users/anharay/go/src/gogenetic/toml/eventstore-read.toml -templateFile=/Users/anharay/go/src/gogenetic/template/go/es-cqrs-read/grpcrestservertemplate.tpl -outDir=eventstore-es-read-output
+go run ./gen/gogentic.go -tomlFile=/Users/anharay/go/src/gogenetic/toml/eventstore-read.toml -templateFile=/Users/anharay/go/src/gogenetic/template/go/es-cqrs-read/cmdservertemplate.tpl -outDir=eventstore-es-read-output
+go run ./gen/gogentic.go -tomlFile=/Users/anharay/go/src/gogenetic/toml/eventstore-read.toml -templateFile=/Users/anharay/go/src/gogenetic/template/go/es-cqrs-read/grpcclienttemplate.tpl -outDir=eventstore-es-read-output
+go run ./gen/gogentic.go -tomlFile=/Users/anharay/go/src/gogenetic/toml/eventstore-read.toml -templateFile=/Users/anharay/go/src/gogenetic/template/go/es-cqrs-read/xormdomaintemplate.tpl -outDir=eventstore-es-read-output
+go run ./gen/gogentic.go -tomlFile=/Users/anharay/go/src/gogenetic/toml/eventstore-read.toml -templateFile=/Users/anharay/go/src/gogenetic/template/go/es-cqrs-read/configtemplate.tpl -outDir=eventstore-es-read-output
+go run ./gen/gogentic.go -tomlFile=/Users/anharay/go/src/gogenetic/toml/eventstore-read.toml -templateFile=/Users/anharay/go/src/gogenetic/template/go/es-cqrs-read/devconftemplate.tpl -outDir=eventstore-es-read-output
+
+cd /Users/anharay/go/src/eventstore-es-read-output
+
+ls
+
+protoc --proto_path=proto -I/usr/local/include -I. \
+  -I$GOPATH/src \
+  -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
+  --go_out=plugins=grpc:proto EventStore-service.proto
+
+
+protoc --proto_path=proto -I/usr/local/include -I. \
+  -I$GOPATH/src \
+  -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
+  --grpc-gateway_out=logtostderr=true:proto EventStore-service.proto
+
+protoc --proto_path=proto  -I/usr/local/include -I. \
+  -I$GOPATH/src \
+  -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
+  --swagger_out=logtostderr=true:proto EventStore-service.proto
+
+gofmt -w /Users/anharay/go/src/eventstore-es-read-output
